12533bb4b880ebf35cef9ccf7a19b2900f8701f9
Stefan Schmelz <35258671+StefanSchmelz@users.noreply.github.com>
2018-07-04 10:39:28 +0200

    Update BruteForce.java

--- BruteForce.java
+++ BruteForce.java
@@ -1,6 +1,16 @@
 class BrurteForce{
-  private CustomPair
-  public BruteForce(int n){
-    
+  private combinedGain;
+
+  public String run(int[] data){
+    int start = 0;
+    int commonGain = 0;
+    int[] gains = new int[];
+    int[] gains = new int[];
+    while(start < data.length - 1){
+     for(int i = start; i < data.length(); ++i){
+       
+     }
+      ++start;
+    }
   }
 }
